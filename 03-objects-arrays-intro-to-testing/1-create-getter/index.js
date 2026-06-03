acbdf89e872f25182bd1852061d44ffe58842404
FlyDream666 <sanek1997_7@mail.ru>
2026-06-04 02:16:02 +0300

    Выполнены задчи ДЗ номер 3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,23 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    // Разбиваем путь на массив ключей
+    const pathArray = path.split('.');
 
+    // Функция для получения значения по заданному пути используем замыкание
+    return function (obj) {
+        let result = obj;
+
+        // Проходим по каждому ключу в пути
+        for (const key of pathArray) {
+            if (Object.hasOwn(result, key)) {
+                result = result[key];
+            } else {
+                return undefined;
+            }
+        }
+
+        // Возвращаем результат
+        return result;
+        };
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,19 @@
  * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    // Если obj не передан, возвращаем undefined
+    if (!obj) {
+        return;
+        }
+    
+    // Создаем новый объект для хранения инвертированной пары ключ-значение
+    const result = {};
 
+    // Перебираем свойства объекта и добавляем их в новый объект с инвертированной парой ключ-значение
+    for (const [key, value] of Object.entries(obj)) {
+        result[value] = key;
+    }
+
+    // Возвращаем новый объект с инвертированной парой ключ-значение
+    return result;
 }
